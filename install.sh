fd443a6955de05e791c6841041541b2ede78a00d
Dem1urg3 <d33m11urge@gmail.com>
2026-05-16 16:30:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,6 +23,39 @@ cat <<EOF > "$APP/linapp-handler.desktop"
 Type=Application
 Name=Linapp Installer
 Exec=$BIN/linapp %f
+MimeType=application/vnd.debian.binary-package;application/gzip;application/x-compressed-tar;application/x-xz;application/x-zstd;application/x-bzip2;
+Terminal=false
+Icon=system-run
+Categories=Utility;System;
+EOF
+
+update-desktop-database "$APP"
+echo "Done. linapp is ready."
+#!/bin/bash
+set -e
+
+BIN="$HOME/.local/bin"
+APP="$HOME/.local/share/applications"
+
+echo "Installing linapp..."
+mkdir -p "$BIN" "$APP" "$HOME/Applications"
+
+if [ -f "linapp" ]; then
+    cp linapp "$BIN/linapp"
+elif [ -f ".local/bin/linapp" ]; then
+    cp .local/bin/linapp "$BIN/linapp"
+else
+    echo "Error: linapp source file not found!"
+    exit 1
+fi
+
+chmod +x "$BIN/linapp"
+
+cat <<EOF > "$APP/linapp-handler.desktop"
+[Desktop Entry]
+Type=Application
+Name=Linapp Installer
+Exec=$BIN/linapp %f
 MimeType=application/vnd.debian.binary-package;application/gzip;application/x-compressed-tar;application/x-xz;application/x-pixman;
 Terminal=false
 Icon=system-run
