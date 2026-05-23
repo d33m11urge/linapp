efc8410fe93b5771858a55d92e684d55b102446c
Dem1urg3 <d33m11urge@gmail.com>
2026-05-23 19:47:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,12 @@ echo "       Linapp System Installer           "
 echo "========================================="
 echo ""
 
-# Checking repository integrity
 if [ ! -d ".local/bin" ]; then
     echo "Error: Directory '.local/bin' not found."
     echo "Please run this script from the root of the cloned repository."
     exit 1
 fi
 
-# Defining system deployment targets
 BIN_DIR="$HOME/.local/bin"
 XDG_APPS_DIR="$HOME/.local/share/applications"
 APPS_DIR="$HOME/Applications"
@@ -33,8 +31,9 @@ echo "-> Generating desktop MIME entry..."
 cat << DESKTOP_EOF > "$XDG_APPS_DIR/linapp-installer.desktop"
 [Desktop Entry]
 Name=Linapp Installer
-Exec=$BIN_DIR/installer.sh %f
-MimeType=application/vnd.debian.binary-package;application/gzip;application/x-compressed-tar;
+# ИСПРАВЛЕНО: Теперь ссылается на твой файл 'linapp' вместо 'installer.sh'
+Exec=$BIN_DIR/linapp %f
+MimeType=application/vnd.debian.binary-package;application/gzip;application/x-compressed-tar;application/x-xz;
 Terminal=false
 Type=Application
 Icon=system-software-install
@@ -44,10 +43,12 @@ DESKTOP_EOF
 
 chmod +x "$XDG_APPS_DIR/linapp-installer.desktop"
 
-echo "-> Registering .deb and .tar.gz file associations..."
+echo "-> Registering file associations..."
 xdg-mime default linapp-installer.desktop application/vnd.debian.binary-package
 xdg-mime default linapp-installer.desktop application/gzip
 xdg-mime default linapp-installer.desktop application/x-compressed-tar
+# ДОБАВЛЕНО: Поддержка формата .xz на уровне ассоциаций
+xdg-mime default linapp-installer.desktop application/x-xz
 
 echo "-> Rebuilding desktop database cache..."
 update-desktop-database "$XDG_APPS_DIR"
